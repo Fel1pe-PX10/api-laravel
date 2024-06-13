28c4f9dec42f136a0e9d22090e39b44450aecf5e
Felipe Paez <ofpaez87@gmail.com>
2024-06-13 19:42:06 +0200

    Migrations, Factories, Seeders and Routes

---
+++ app/Http/Controllers/Api/V1/InvoceController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\Invoce;
+
+use App\Http\Controllers\Controller;
+
+use App\Http\Requests\StoreInvoceRequest;
+use App\Http\Requests\UpdateInvoceRequest;
+
+class InvoceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInvoceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Invoce $invoce)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Invoce $invoce)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateInvoceRequest $request, Invoce $invoce)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Invoce $invoce)
+    {
+        //
+    }
+}
