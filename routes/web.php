e01360a7424d550b5261236299b68f6e58b38ac6
Felipe Paez <ofpaez87@gmail.com>
2024-06-19 16:07:02 +0200

    Auth: implmenenting sanctum tokens

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,44 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('setup', function () {
+    $credentials = [
+        'email' => 'admin@admin.com',
+        'password' => 'password'
+    ];
+
+    if (!Auth::attempt($credentials))
+    {
+        $user = new User();
+
+        $user->name = 'Admin';
+        $user->email = $credentials['email'];
+        $user->password = Hash::make($credentials['password']);
+
+        $user->save();
+
+        if (Auth::attempt($credentials))
+        {
+            $user = User::where('email', $credentials['email'])->first();
+
+            $adminToken = $user->createToken('admin-token', ['create', 'update', 'delete']);
+            $updateToken = $user->createToken('admin-token', ['create', 'update', 'delete']);
+            $basicToken = $user->createToken('admin-token', ['create', 'update', 'delete']);
+            
+            return [
+                'admin' => $adminToken->plainTextToken,
+                'update' => $updateToken->plainTextToken,
+                'basic' => $basicToken->plainTextToken
+            ];
+        }
+    }
 });
